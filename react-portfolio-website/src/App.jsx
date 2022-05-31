abc79c1599ca4e53ce6d9902fccf6fc21a67edfd
Sebastian Rivera <srvera@uiowa.edu>
2022-05-31 13:10:23 -0500

    added some styling

--- react-portfolio-website/src/App.jsx
+++ react-portfolio-website/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Portfolio />
+    <Footer />
+    </>
   )
 }
 
